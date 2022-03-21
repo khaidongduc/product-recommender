2609962bb20d61b3b7211400c9d74b59c29e4a14
khaidongduc <khaidongduc@gmail.com>
2022-03-21 10:35:46 -0400

    add starter file
    add starter files for the project

---
+++ rs-content-based/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rs_content_based
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
